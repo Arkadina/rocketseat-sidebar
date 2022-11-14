473bd043b4ebe73e4f715d8ca3c0594f7149e9e6
Arkadina <61208486+Arkadina@users.noreply.github.com>
2022-11-14 19:49:14 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Desafio Rocketseat - Countdown
+
+<a href="https://efficient-sloth-d85.notion.site/Desafio-Countdown-4572ce6f5c91469abe0171f454a13e3f">Acessar desafio.</a>
+
+<img src="https://i.ibb.co/jMnCnBJ/img.png">
+
---
+++ js/index.js
@@ -0,0 +1,54 @@
+const dayP = document.querySelector(".day p");
+const hoursP = document.querySelector(".hours p");
+const minutesP = document.querySelector(".minutes p");
+const secondsP = document.querySelector(".seconds p");
+
+var day = 0;
+var hours = 0;
+var minutes = 1;
+var seconds = 1;
+var countInterval = null;
+
+function countdown() {
+    countInterval = setInterval(setTime, 1000);
+}
+
+function setP() {
+    dayP.innerText = day;
+    hoursP.innerText = hours;
+    minutesP.innerText = minutes;
+    secondsP.innerText = seconds;
+}
+
+function setTime() {
+    seconds--;
+    if (seconds <= 0) {
+        if (day == 0 && seconds == 0 && minutes == 0 && hours == 0) {
+            clearInterval(countInterval);
+        } else {
+            seconds = 60;
+            if (minutes > 0) {
+                minutes--;
+            }
+        }
+    }
+
+    if (minutes <= 0) {
+        if (hours > 0) {
+            minutes = 60;
+            hours--;
+        }
+    }
+
+    if (hours <= 0) {
+        if (day > 0) {
+            hours = 24;
+            day--;
+        }
+    }
+    setP();
+    console.log(day, hours, minutes, seconds);
+}
+
+setP();
+countdown();
